2a8f8cdbdc78ccf7d1a3533e714afb6e886ab0bb
mtkranmini <135610219+mtkranmini@users.noreply.github.com>
2024-09-27 12:59:54 +0530

    kavi

---
+++ src/Components/card.jsx
@@ -0,0 +1,65 @@
+const card = () => {
+    return (
+      <div className="items-center justify-center gap-6 mt-8 mb-6 mx10 lg:flex-row lg:items-center lg:justify-between lg:gap-20 text-primary">
+         <div>
+          <h1 className="text-2xl font-bold text-center">Caring is the new marketing</h1><br></br>
+          <p className="text-center">
+          The Nexcent blog is the best place to read about the latest membership insights, <br></br>trends and more. See who is joining the community,<br></br> read about how our community
+          are increasing their membership income and lot is more.
+            </p>
+  </div>
+  <div>
+  <div className="flex flex-wrap justify-center gap-1 p-5 mb-40">
+  <div className="relative z-10 max-w-xs mx-auto mb-10 sm:gap-10"> 
+          <img src="src/assets/image 18.png" width={300} height={400} alt="" />
+          <div className="rounded-lg ">
+          <div className="absolute left-0 right-0 p-4 mx-5 mb-3 top-32 rounded-2xl bg-slate-100 ">
+              
+              <p className="text-center ">  Creating Streamlined<br></br> Safeguarding Processes with<br></br> OneRen</p>
+              <a href="#" className="flex items-center justify-center mt-3 text-secondary">Readmore <img src="src/assets/right 2.png" alt="" /></a>
+          </div> 
+          </div>
+      </div>
+      <div className="relative z-10 max-w-xs mx-auto mb-10 ">
+          <img src="src/assets/image 19.png" width={300} height={400} alt="" />
+          <div className="w-full h-auto rounded-lg">
+          <div className="absolute left-0 right-0 p-4 mx-5 mb-3 top-32 rounded-2xl bg-slate-100 ">
+  
+              <p className="text-center"> What are your safeguarding <br></br>responsibilities and how can <br></br>you manage them?
+              </p>
+              
+              <a href="#" className="flex items-center justify-center mt-3 text-secondary">Readmore <img src="src/assets/right 2.png" alt="" /></a>
+             
+          </div>
+          </div>
+      </div>
+      <div className="relative z-10 max-w-xs mx-auto mb-10">
+          <img src="src/assets/image 20.png" width={300} height={400} alt="" />
+          <div className="w-full h-auto rounded-lg">
+          <div className="absolute left-0 right-0 p-4 mx-5 mb-3 top-32 rounded-2xl bg-slate-100">
+  
+              <p className="text-center">Revamping the Membership<br></br> Model with Triathlon<br></br> Australia
+              </p>
+              <a href="#" className="flex items-center justify-center mt-3 text-secondary">Readmore <img src="src/assets/right 2.png" alt="" /></a>
+          </div>
+          </div>
+      </div>
+      </div>
+  </div>
+  
+      </div>
+      
+  
+  
+              
+         
+    
+  
+  
+  
+  
+     
+    )
+  }
+  
+  export default card
---
+++ src/Components/get.jsx
@@ -0,0 +1,15 @@
+
+const get = () => {
+  return (
+    <div className="items-center justify-center p-3 lg:flex-row lg:items-center lg:justify-between lg:gap-20 bg-slate-100">
+    <h1 className="text-5xl font-bold text-center ">Pellentesque suscipit<br></br> fringilla libero eu.</h1><br></br> 
+    
+    <div className="flex flex-row justify-center mt-5 mb-8" >
+    <a href="# "
+      className="flex flex-row px-6 py-2 mt-4 font-medium text-center border-2 bg-secondary rounded-1xl"
+    >Get a Demo<img src="src/assets/Right.png" alt="" /> </a></div>
+  </div>
+  )
+}
+
+export default get
